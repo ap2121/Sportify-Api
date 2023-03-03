ca11d8073ba132297abe4c69bc4ea706ead60af9
Arthur Pate <arthurpate42@gmail.com>
2023-03-03 12:51:22 -0800

    middleware functions done and auth controller skeleton set up

---
+++ controllers/AuthController.js
@@ -0,0 +1,17 @@
+const { user } = require('../models')
+
+const Login = async (req, res) => {
+    try {
+
+    } catch(error) {
+        throw error
+    }
+}
+
+const Register = async () => {
+    try {
+
+    } catch (error) {
+        throw error
+    }
+}
