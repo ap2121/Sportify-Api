b158aecd79d94beea27e2c4bc19cef4d01b74ea7
Arthur Pate <arthurpate42@gmail.com>
2023-03-08 08:19:19 -0800

    before pulling

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,15 +1,11 @@
 const Router = require('express').Router()
 const controller = require('../controllers/PostController')
-const middleware = require('../middleware')
 Router.get('/posts-by-sport/:sport_id', controller.getPostsBySport)
 Router.post('/create-post/:user_id/:sport_id', controller.createPost)
 Router.get('/get-post/:post_id', controller.getPostById)
 Router.delete('/delete-post/:post_id', 
-middleware.stripToken,
-middleware.verifyToken,
 controller.deletePost)
 Router.put('/edit-post/:post_id', 
-middleware.stripToken,
-middleware.verifyToken,
 controller.updatePost)
 module.exports = Router
+
