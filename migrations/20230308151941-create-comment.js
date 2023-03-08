2b393c36925b2cfd02b5690afdeca1efbc7ff667
Nickon Babaei <babaein1@gmail.com>
2023-03-08 12:36:19 -0300

    implemented comment model

---
+++ migrations/20230308151941-create-comment.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      postId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'posts',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comments');
+  }
+};
--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,5 +1,6 @@
 const Router = require('express').Router()
 const controller = require('../controllers/PostController')
+const middleware = require('../middleware')
 
 Router.get('/posts-by-sport/:sport_id', controller.getPostsBySport)
 Router.post('/create-post/:user_id/:sport_id', controller.createPost)
