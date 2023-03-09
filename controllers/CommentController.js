2a0a806170940fbcb2aa9267ac38d6137fc43727
Nickon Babaei <babaein1@gmail.com>
2023-03-09 18:01:09 -0300

    added get all comments controller

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -13,6 +13,15 @@ const getCommentsByPost = async (req, res) => {
     }
 }
 
+const getAllComments = async (req, res) => {
+    try {
+        const comments = await Comment.findAll({})
+        res.send(comments)
+    } catch(error) {
+        throw error
+    }
+}
+
 const createComment = async (req, res) => {
     try {
         const userId = req.params.user_id
@@ -43,4 +52,5 @@ module.exports = {
     getCommentsByPost,
     createComment,
     deleteComment,
+    getAllComments
 }
